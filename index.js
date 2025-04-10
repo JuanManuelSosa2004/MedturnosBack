55172126539f0b27131b9f11e21df72e2d0723c7
Juan Manuel <juanmanuelsvccnt@gmail.com>
2025-04-10 19:26:37 -0300

    Correccion minima de funcionalidad para test de endpoints de medico

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const PORT=3000;
 
 const usuarioRoutes = require('./routes/usuariosRoutes');
 //const turnosRoutes = require('./routes/turnosRoutes');
-//const medicosRoutes = require('./routes/medicosRoutes');
+const medicosRoutes = require('./routes/medicosRoutes');
 
 
 app.use(express.json());
 app.use('/usuarios', usuarioRoutes); 
 //app.use('/turnos', turnosRoutes);
-//app.use('/medicos', medicosRoutes);
+app.use('/medicos', medicosRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
