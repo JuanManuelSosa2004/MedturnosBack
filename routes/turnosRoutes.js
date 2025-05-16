c431d85f93fe530e3d233d5ab16f26613c26aab3
Juan Sosa <juanmanuelsvccnt@gmail.com>
2025-05-16 11:47:52 -0300

    Filling commit

--- routes/turnosRoutes.js
+++ routes/turnosRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const TurnosController = require('../controllers/turnos.controller');
-
+//visualizacion
 router.get('/disponibles', TurnosController.getDisponibles);
 router.get('/disponibles/especialidad/:especialidad', TurnosController.getByEspecialidad);
 router.get('/disponibles/profesional/:profesional_id', TurnosController.getByProfesional);
+
+//accion
+router.post('/:id/reservar', TurnosController.reservarTurno);
+router.delete('/:id', TurnosController.cancelarTurno);
 
 module.exports = router;
