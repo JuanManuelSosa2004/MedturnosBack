1e0433d2d1b2ceaeff7e7bd53f13d35f6f914368
agusmel <agusmel03@live.com.ar>
2025-05-23 09:33:51 -0300

    implementacion de pool de conexiónes

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
 const mysql = require('mysql2');
 
 // Usar variables de entorno para la conexión
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10, // Puedes ajustar este número según tus necesidades
+  queueLimit: 0
 });
 
 // Conectar a la base de datos
@@ -19,4 +22,4 @@ connection.connect(err => {
 });
 
 // Exportar la conexión
-module.exports = connection;
+module.exports = pool;
