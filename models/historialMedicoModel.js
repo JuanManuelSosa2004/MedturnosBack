f9f01b2d9290170c2d48a3ff0d6732bb9e810567
JuanManuelSosa2004 <juanmanuelsvccnt@gmail.com>
2025-05-26 16:47:07 -0300

    Reparaciones del anterior commit

--- models/historialMedicoModel.js
+++ models/historialMedicoModel.js
@@ -74,7 +74,45 @@ const obtenerTurnosCancelados = (id_usuario, callback) => {
   });
 };
 
+const obtenerTurnosProgramados = (id_usuario, callback) => {
+  const query = `
+    SELECT 
+      t.id_turno,
+      t.disponibilidad,
+      t.fecha,
+      t.hora,
+      p.nombre_profesional,
+      p.ubicacion,
+      p.diasTrabajo,
+      e.descripcion AS especialidad
+    FROM turnos t
+    JOIN profesionales p ON t.id_profesional = p.id_profesional
+    JOIN especialidad e ON p.id_especialidad = e.id_especialidad
+    WHERE t.id_usuario = ?
+      AND t.disponibilidad = 'reservado'
+    ORDER BY t.fecha ASC, t.hora ASC
+  `;
+  db.query(query, [id_usuario], (err, resultados) => {
+    if (err) return callback(err);
+
+    const turnos = resultados.map((turno) => ({
+      ...turno,
+      diasTrabajo: (() => {
+        try {
+          return JSON.parse(turno.diasTrabajo || '[]');
+        } catch (error) {
+          console.error('Error al parsear diasTrabajo:', error);
+          return [];
+        }
+      })(),
+    }));
+
+    callback(null, turnos);
+  });
+};
+
 module.exports = {
   obtenerTurnosRealizados,
   obtenerTurnosCancelados,
+  obtenerTurnosProgramados,
 };
